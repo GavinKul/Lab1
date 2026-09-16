fad41c3d034dfdf790732244e573274b6a673213
GavinKul <gkulch@my.ccsu.edu>
2026-09-16 13:55:05 -0400

    Implement FizzBuzz function in HWLab1.c

--- HWLab1.c
+++ HWLab1.c
@@ -1,1 +1,29 @@
 
+#include <stdio.h>
+
+/* Function that prints the correct FizzBuzz output for one integer */
+void fizzbuzz_single(int n) {
+    if (n % 15 == 0) {
+        printf("FizzBuzz\n");
+    } else if (n % 3 == 0) {
+        printf("Fizz\n");
+    } else if (n % 5 == 0) {
+        printf("Buzz\n");
+    } else {
+        printf("%d\n", n);
+    }
+}
+
+int main() {
+    int limit;
+
+    printf("Enter an integer: ");
+    scanf("%d", &limit);
+
+    /* Loop from 1 to the entered number */
+    for (int i = 1; i <= limit; i++) {
+        fizzbuzz_single(i);
+    }
+
+    return 0;
+}
